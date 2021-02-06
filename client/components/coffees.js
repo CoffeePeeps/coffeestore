6629fec384d6f3103d889d2d4dda1ff088f5062b
Michael Rios <michael.rios093@gmail.com>
2021-02-06 10:37:13 -0500

    have a basic react component to list all coffees happy to change it

---
+++ client/components/coffees.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+const Coffees = ( {coffees} ) => {
+    // console.log(coffees);
+    
+    return(
+        <div className = { 'list' }>
+            <ul>
+            {  
+                coffees.map( coffee => { 
+                    return (
+                    <li key={ coffee.id }>
+                        <Link to={`/coffees/${ coffee.id }`}>
+                            { coffee.name } 
+                        </Link>
+                    </li>
+                );
+            })
+            }
+        </ul>
+        </div>
+    )
+}
+
+const mapStateToProps = (state)=> {
+    return state;
+};
+
+//call loadStudents here, now need to add a load async
+//nick showed me how to simplfy the logic, don't have time to impliment it but hope to go back to it latter  
+const mapDispatchToProps = (dispatch) => {
+    return {
+      bootstrap: ()=> {
+        //may need to change the name   
+        dispatch(loadCoffees());
+      }
+    };
+  }
+
+export default connect(mapStateToProps, mapDispatchToProps)(Coffees);
