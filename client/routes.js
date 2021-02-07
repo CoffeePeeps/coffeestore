85b227b5f145749f36d9671b38da3c6881031431
Jacob Goldstein <by.jakeg@gmail.com>
2021-02-06 22:11:21 -0500

    added cart component

--- client/routes.js
+++ client/routes.js
@@ -1,7 +1,7 @@
 import React, {Component, Fragment} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
-import {Login, Signup, Home} from './components'
+import {Login, Signup, Home, Cart} from './components'
 import {me} from './store'
 
 /**
@@ -20,7 +20,8 @@ class Routes extends Component {
         {isLoggedIn ? (
           <Switch>
             <Route path="/home" component={Home} />
-            <Redirect to="/home" />
+            <Route path="/cart" component={Cart}/>
+            {/* <Redirect to="/home" /> */}
           </Switch>
         ) : (
           <Switch>
