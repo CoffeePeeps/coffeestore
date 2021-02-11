537bf207db45cb3b91189fb68db7e97eaee52e0d
Jacob Goldstein <by.jakeg@gmail.com>
2021-02-10 21:25:37 -0500

    added /api/checkout/... put request logic. can now close a cart and start a new one with a successful payment

--- server/api/checkout.js
+++ server/api/checkout.js
@@ -1,15 +1,28 @@
 const router = require('express').Router();
-const { models: { Coffee, User  }} = require('../db');
+const { models: { Coffee, User, Cart  }} = require('../db');
 module.exports = router;
 
-// If the user is signed in, check to see if they have a stored address. 
+// If the user is signed in, check to see if they have a stored address.
 // If they do, get it and auto-populate
 // If they don't, store it
 
-router.get('/', async (req, res, next) => {
-    try{
-        
-    } catch(ex) {
-        next(ex);
+// cart checkout happens on put request, update existing cart open to false
+// assign that user a new empty cart
+
+router.put("/:uid/:cartId", async (req, res, next) => {
+    try {
+        const cart = await Cart.findOne({
+            where: {userId: req.params.uid, id: req.params.cartId}
+        })
+
+        if(req.body.payment === "success"){
+            cart.open = false
+            await Cart.create({userId: req.params.uid})
+            await cart.save()
+        }
+
+        res.sendStatus(204)
+    } catch (ex) {
+        next(ex)
     }
 })
