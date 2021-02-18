aca27dc0a18dd8999c2e039f7851fc27e1acf3b6
Jared Suresky <jsuresky@gmail.com>
2021-02-17 20:55:00 -0500

    Coffee image assets and coffee component

--- client/components/Coffee.js
+++ client/components/Coffee.js
@@ -1,112 +1,146 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addNewCoffee, updatedStock } from "../store";
+import * as mdb from "mdb-ui-kit"; // lib
 
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import {addNewCoffee, updatedStock} from '../store'
+class Coffee extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quantity: 1,
+    };
 
+    this.putInCart = this.putInCart.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
 
-class Coffee extends Component{
-    constructor(props){
-      super(props)
-      this.state = {
-        quantity: 1
-      };
-    
-      this.putInCart = this.putInCart.bind(this);
-      this.onChange = this.onChange.bind(this);
-    }
-    
-    onChange(ev){
-      const change = {};
-      change[ev.target.name] = ev.target.value;
-      this.setState(change);
-    }
+  onChange(ev) {
+    const change = {};
+    change[ev.target.name] = ev.target.value;
+    this.setState(change);
+  }
 
-    putInCart(coffeeId, coffeeStock){
-        
-      //make sure it is type number and a whole number
-      let quantity = Math.ceil(this.state.quantity * 1)
-      
-      // if it's not a positive integer just make it 1
-      // should show a message 
-      if (isNaN(quantity) || quantity < 1 ){
-          quantity = 1;
-      } 
-      
-      //can't sell more than we have 
-      if (quantity > coffeeStock){
-        quantity = coffeeStock * 1;
-      }
+  putInCart(coffeeId, coffeeStock) {
+    //make sure it is type number and a whole number
+    let quantity = Math.ceil(this.state.quantity * 1);
 
-      let stock = coffeeStock - quantity;
-        
-      this.props.updateStock(stock, coffeeId)
-      // we are out of stock can't put it in the cart
-      if (quantity > 0) {      
-        this.props.addNewCoffee(quantity, this.props.auth.id, coffeeId);
-      } 
+    // if it's not a positive integer just make it 1
+    // should show a message
+    if (isNaN(quantity) || quantity < 1) {
+      quantity = 1;
     }
 
-    render(){
-        const coffee = this.props.coffee;
-        const { quantity } = this.state;
-        const { onChange } = this;
+    //can't sell more than we have
+    if (quantity > coffeeStock) {
+      quantity = coffeeStock * 1;
+    }
 
-        if(!coffee.id){
-            return '...loading coffee';
-        }
-        return (
-          <body>
-            <div>
-              <header>
-                <h1 id="prodDescription"> Product Description ☕ </h1>
-                <h2>{coffee.name} </h2>
-      
-                <hr />
-              </header>
-              <main>
-                <h4>Details</h4>
-                {coffee.description && `description: ${coffee.description}`}
-                <br />
-                {coffee.price && `price: ${coffee.price}`}
-                <br />
-                { coffee.stock > 0 ? (
-                <div>
-                  {coffee.stock && `stock: ${coffee.stock}`}
-                  <br />
-                  <input name='quantity' value={ quantity } onChange = { onChange }/>
-                  <button onClick = {()=> this.putInCart(`${coffee.id}`, `${coffee.stock}`)}>add to cart</button>
-                </div>
-                ) : (
-                  <p>out of stock</p>
-                )}
-              </main>
-              <a href = '/'>back to main page</a>
+    let stock = coffeeStock - quantity;
+
+    this.props.updateStock(stock, coffeeId);
+    // we are out of stock can't put it in the cart
+    if (quantity > 0) {
+      this.props.addNewCoffee(quantity, this.props.auth.id, coffeeId);
+    }
+  }
+
+  render() {
+    const coffee = this.props.coffee;
+    const { quantity } = this.state;
+    const { onChange } = this;
+
+    if (!coffee.id) {
+      return "...loading coffee";
+    }
+    return (
+      <main className="mt-5 pt-4">
+        <div className="container dark-grey-text mt-5">
+          <div className="row wow fadeIn">
+            <div className="col-md-6 mb-4">
+              <img
+                src={window.location.origin + "/assets/delicious-coffee.jpg"}
+                className="img-fluid"
+                alt=""
+              />
             </div>
-          </body>
-        );
-      };  
-    }
-  
-  
+
+            <div className="col-md-6 mb-4">
+              <div className="p-4">
+                <p className="lead">
+                  <span className="mr-1"></span>
+                  <span>${coffee.price}</span>
+                </p>
+
+                <p className="lead font-weight-bold">{coffee.description}</p>
+                <h2></h2>
+
+                <p>
+                  This is the description of the product: {coffee.description}
+                </p>
+                <br />
+                {coffee.stock > 0 ? (
+                  <div>
+                    {coffee.stock && `stock: ${coffee.stock}`}
+                    <br />
+                    <input
+                      name="quantity"
+                      value={quantity}
+                      onChange={onChange}
+                    />
+
+                    <button
+                      onClick={() =>
+                        this.putInCart(`${coffee.id}`, `${coffee.stock}`)
+                      }
+                    >
+                      Add to Cart
+                    </button>
+                  </div>
+                ) : (
+                  <p>
+                    Sorry, this product is out of stock. Please check back
+                    later.
+                  </p>
+                )}
+                {/* <form className="d-flex justify-content-left">
+              
+              <input type="number" value="1" aria-label="Search" className="form-control" style="width: 100px">
+              <button className="btn btn-primary btn-md my-0 p" type="submit">Add to cart
+                <i className="fas fa-shopping-cart ml-1"></i>
+              </button>
+
+            </form> */}
+              </div>
+            </div>
+          </div>
+
+          <hr />
+        </div>
+      </main>
+    );
+  }
+}
+
 export default connect(
-    ( {auth, cartList, product}, otherProps)=> {
-        const coffee = product.find(coffee => coffee.id === otherProps.match.params.id * 1) || {};
-        return {
-            coffee,
-            auth,
-            cartList,
-            product
-            };
-        },(dispatch) => {  
-            return {
-            addNewCoffee(quantity, userId, coffeeId){
-                dispatch(addNewCoffee(quantity, userId, coffeeId))
-            },    
-            updateStock(stock, coffeeId){
-              dispatch(updatedStock(stock, coffeeId))
-            },
-          }
-        }
-        
-    )(Coffee);
-    
+  ({ auth, cartList, product }, otherProps) => {
+    const coffee =
+      product.find((coffee) => coffee.id === otherProps.match.params.id * 1) ||
+      {};
+    return {
+      coffee,
+      auth,
+      cartList,
+      product,
+    };
+  },
+  (dispatch) => {
+    return {
+      addNewCoffee(quantity, userId, coffeeId) {
+        dispatch(addNewCoffee(quantity, userId, coffeeId));
+      },
+      updateStock(stock, coffeeId) {
+        dispatch(updatedStock(stock, coffeeId));
+      },
+    };
+  }
+)(Coffee);
