9efcb09c29bc62710cb23bac4f683ef6e65424b1
Michael Rios <michael.rios093@gmail.com>
2021-02-18 10:56:55 -0500

    with working routes.js

--- client/routes.js
+++ client/routes.js
@@ -1,7 +1,80 @@
+// import React, {Component, Fragment} from 'react'
+// import {connect} from 'react-redux'
+// import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
+// import {Login, Signup, Home, User, SingleOrder, Cart, Coffee,
+//         AdminSingleProductView, AdminAllProductView, AdminAddNewProduct} from './components/index'
+// import {me} from './store'
+
+// /**
+//  * COMPONENT
+//  */
+// class Routes extends Component {
+//   componentDidMount() {
+//     this.props.loadInitialData()
+//   }
+
+//   render() {
+//     const {isLoggedIn} = this.props;
+//     return (
+
+//       <div>
+//         {isLoggedIn ? (
+//           <Switch>
+//             <Route path="/home" component={Home} />
+//             <Route path="/cart" component={Cart}/>
+//             <Route component={ Coffee } path = '/coffee/:id' exact/>
+//             <Redirect to="/home" />
+//           </Switch>
+
+
+//         ) : (
+//           <Switch>
+//             <Route path="/login" component={Login} />
+//             <Route path="/signup" component={Signup} />
+
+//             <Route path="/user/:userId" component={User} />
+//             <Route path="/orderHistory/:userId/:orderId" component={SingleOrder} />
+
+//             <Route path="/admin/allCoffees" component = {AdminAllProductView}/>
+//             <Route path="/admin/singleCoffee/:coffeeId" component = {AdminSingleProductView}/>
+//             <Route path="/admin/addNewCoffee" component = {AdminAddNewProduct}/>
+//             <Redirect to="/login" />
+//           </Switch>
+//         )}
+//       </div>
+
+
+//     )
+//   }
+// }
+
+// /**
+//  * CONTAINER
+//  */
+// const mapState = state => {
+//   return {
+//     // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
+//     // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
+//     isLoggedIn: !!state.auth.id
+//   }
+// }
+
+// const mapDispatch = dispatch => {
+//   return {
+//     loadInitialData() {
+//       dispatch(me())
+//     }
+//   }
+// }
+
+// // The `withRouter` wrapper makes sure that updates are not blocked
+// // when the url changes
+// export default withRouter(connect(mapState, mapDispatch)(Routes))
+
 import React, {Component, Fragment} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
-import {Login, Signup, Home, User, SingleOrder, Cart, Coffee,
+import {Login, Signup, Home, User, SingleOrder, Cart, Coffee, 
         AdminSingleProductView, AdminAllProductView, AdminAddNewProduct} from './components/index'
 import {me} from './store'
 
@@ -14,17 +87,14 @@ class Routes extends Component {
   }
 
   render() {
-
     const {isLoggedIn, isAdmin} = this.props;
+    console.log('HOME PROPS!!')
+    console.log(this.props);
 
     return (
-      //Comment to update
+
       <div>
-       
-        
-      
-      
-      {isAdmin ? (
+        {isAdmin ? (
         <Switch>
             <Route path="/home" component={Home} />
             <Route path="/cart" component={Cart}/>
@@ -64,10 +134,12 @@ class Routes extends Component {
  * CONTAINER
  */
 const mapState = state => {
+  console.log(state);
   return {
     // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
     // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
-    isLoggedIn: !!state.auth.id
+    isLoggedIn: !!state.auth.id,
+    isAdmin: (state.auth.typeOfUser === 'ADMIN')
   }
 }
 
